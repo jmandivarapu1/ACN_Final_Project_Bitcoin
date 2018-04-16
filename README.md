1dbb0520a60e677e9f064ded592be7b90b690a32
JayaKrishna Mandivarapu <jayanayana0@gmail.com>
2018-04-16 16:00:00 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,31 @@
+Team-BTC2- Jaya Krishna M and Ramya T and Hemanth
+
+
+Files and Description:
+
+Transactor File which defines all the needed properties required for a basic transaction such as the Public and Private Keys of the User
+Modules:
+1. Clients
+    a. Send packets for a transaction to another client
+    b. Receive transaction from another client
+    c. Broadcast the transaction to all other clients to encrypt the transaction data
+    d. Encrypt the transaction data coming from another client
+
+2. Routers
+    a. Define listener and sender protocols
+    b. Structure to drop randomly packets
+
+3. Encryption and Client Token:
+    a. Encrypt incoming regular transaction
+    b. Token to hold the info of client, transactions and winner coins.
+
+
+Tasks of The Project:
+
+    • Understand the concept of blockchain based transaction system
+    • Define the blueprint of each module
+    • Implement the network system based on TCP/IP communication protocols
+    • Demonstrate the project implementation
+    • Submit your project report with performance evaluation
+
+
---
+++ block.py
@@ -0,0 +1,58 @@
+import hashlib
+
+
+class Block:
+    """"Implementation of a Block Object"""
+
+    # Constructor
+    def __init__(self, blockchain_in, timestamp_in, transactions_in):
+        """Constructing base block instance."""
+        # Block Height & Transactions
+        self.block_height = blockchain_in[-1].block_height + 1    #Basically, The Index in Our Blockchain
+        self.transactions = transactions_in
+        self.transaction_counter = len(transactions_in)
+
+        # Block Header
+        self.previous_hash = blockchain_in[-1].hash
+        self.timestamp = timestamp_in
+
+        # Block's Hash: Made by Hashing Block Header 2x Through Cryptographic Hash Function
+        header_string = str(self.previous_hash).encode('utf-8') + str(self.timestamp).encode('utf-8')
+        self.hash = hashlib.sha256(str(hashlib.sha256(header_string).hexdigest()).encode('utf-8'))
+
+    # For Clean Output
+    def __str__(self):
+        """Print properly."""
+        output = "Block Height: " + str(self.block_height) + "\n"
+        output += "Block Timestamp: " + str(self.timestamp) + "\n"
+        output += "Block Hash: " + str(self.hash.hexdigest()) + "\n"
+        output += "Block Previous Hash: " + str(self.previous_hash.hexdigest()) + "\n"
+        return output
+
+
+class GenesisBlock():
+    """"Implementation of a Genesis Block"""
+
+    def __init__(self):
+        """Constructing base genesis block instance."""
+        # Block Height & Transactions
+        self.block_height = 0
+        self.transactions = []
+        self.transaction_counter = 0
+
+        # Block Header
+        self.previous_hash = None
+        self.timestamp = None
+
+        # Block's Hash: Made by Hashing Block Header 2x Through Cryptographic Hash Function
+        hash_text = "Genesis Block".encode('utf-8')
+        self.hash = hashlib.sha256(str(hashlib.sha256(hash_text).hexdigest()).encode('utf-8'))
+
+    # For Clean Output
+    def __str__(self):
+        """Print properly."""
+        output = ""
+        output += "Block Timestamp: Genesis\n"
+        output += "Block Hash: " + str(self.hash.hexdigest()) + "\n"
+        output += "Block Previous Hash: N/A\n"
+        return output
